968b06cb939445c1ccb504f13f6e4efac1f7b03b
emddutton <emddutton@gmail.com>
2016-04-04 09:31:33 -0600

    still working on it

--- intermediate_projects/show-local-weather /js/index.js
+++ intermediate_projects/show-local-weather /js/index.js
@@ -11,20 +11,61 @@ function success(pos){
   var city = data.name;
   var imperialTemp = Math.round(data.main.temp);
   var celsius = Math.round((imperialTemp - 32) / 1.8);
-  var detail = data.weather[0].main;
-  $(".city").text(city);
-    $(".temp").addClass("imperial").html(imperialTemp + "&deg; F");
-    $("button").append(" Celsius");
+  var detail = data.weather[0].main.toLowerCase();
     
- $("button").on("click", function(){
- if($(".temp").hasClass("imperial")) {  $(".temp").empty().removeClass("imperial").addClass("metric").html(celsius + "&deg; C");
-                                      $(this).empty().append("Switch to Farenheit");
- }
-      else if($(".temp").hasClass("metric")) {
-        $(".temp").empty().removeClass("metric").addClass("imperial").html(imperialTemp + "&deg; F");
-        $(this).empty().append("Switch to Celsius");
+   var prettyPics = {
+      clouds: {
+        background: "http://res.cloudinary.com/dgtkvm9hr/image/upload/v1459632686/cloudy_wc1mgq.jpg",
+        icon: "http://res.cloudinary.com/dgtkvm9hr/image/upload/v1459657480/Cloud_rafiyw.png"
+      },
+      clear: {
+        background: "http://res.cloudinary.com/dgtkvm9hr/image/upload/v1459632674/clear_zbptzw.jpg",
+        icon: "http://res.cloudinary.com/dgtkvm9hr/image/upload/v1459657480/Sun_ohyynn.png"
+      },
+      rain: {
+        background: "http://res.cloudinary.com/dgtkvm9hr/image/upload/v1459632689/rain_ewdxfs.jpg",
+        icon: "http://res.cloudinary.com/dgtkvm9hr/image/upload/v1459657480/Rain_he25cj.png"
+      },
+      snow: {
+        background: "http://res.cloudinary.com/dgtkvm9hr/image/upload/v1459632687/snow_n9uuth.jpg",
+        icon: "http://res.cloudinary.com/dgtkvm9hr/image/upload/v1459632687/snow_n9uuth.jpg"
+      },
+      default: {
+        background: "http://res.cloudinary.com/dgtkvm9hr/image/upload/v1459701011/default_p8htre.jpg",
+        icon: "http://res.cloudinary.com/dgtkvm9hr/image/upload/v1459657480/PartlySunny_gbx368.png"
       }
-   })
+    };
+    
+   for (var key in prettyPics) {
+    if (!prettyPics.hasOwnProperty(key)) continue;
+    var obj = prettyPics[key];
+    for (var prop in obj) {
+  if(!obj.hasOwnProperty(prop)) continue;
+      console.log(obj.background);
+    }
+}
+    
+    $("body").attr("id", detail);
+    
+//     if(detail == "clouds") {
+      
+//       $("body").css("background", "url('http://res.cloudinary.com/dgtkvm9hr/image/upload/v1459632686/cloudy_wc1mgq.jpg') center no-repeat");
+//     }
+    
+$(".city").text(city);
+ 
+$(".detail").html(detail);
+    $(".temp").addClass("imperial").html(imperialTemp + "&deg; F");
+$("button").append(" Celsius");
+    
+$("button").on("click", function(){
+ if($(".temp").hasClass("imperial")) {$(".temp").empty().removeClass("imperial").addClass("metric").html(celsius + "&deg; C");
+                                      $(this).empty().append("Switch to Farenheit");
+}
+else if($(".temp").hasClass("metric"))
+{$(".temp").empty().removeClass("metric").addClass("imperial").html(imperialTemp + "&deg; F");
+        $(this).empty().append("Switch to Celsius");}
+}) //end button click
     
 }) // end getJSON
 } // end success
