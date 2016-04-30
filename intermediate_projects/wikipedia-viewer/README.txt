745702d6e18d21c122f8b9eed84aa62dab4480d3
emddutton <emddutton@gmail.com>
2016-04-30 10:30:30 -0600

    new wikipedia project

---
+++ intermediate_projects/wikipedia-viewer/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/emddutton/pen/pypXxQ.
+
+ Part of the Intermediate Front End Projects on FreeCodeCamp
---
+++ intermediate_projects/wikipedia-viewer/js/index.js
@@ -0,0 +1,25 @@
+var toSearch;
+$(".searchField").on('keyup click', function (e) {
+    if (e.keyCode == 13) {
+        toSearch = $("#search").val();
+    }
+
+  $.ajax({
+  url: '//en.wikipedia.org/w/api.php?format=json&action=query',
+  data: { action: 'query', list: 'search', srsearch: toSearch, prop: 'extracts' },
+  dataType: 'jsonp',
+  success: function (x) {
+    console.log(x);
+ console.log(x.query.search.length);
+    for(var i = 0; i < x.query.search.length; i++) {
+    var title = x.query.search[i].title;
+    console.log(title);
+    var snippet = x.query.search[i].snippet;
+    console.log(snippet);
+    var baseurl = "http://en.wikipedia.org/wiki/"; 
+   $(".results").append('<p>' + title + '</p>' + '<p>' + snippet + '</p>');   
+    } 
+  }
+});
+  
+});
